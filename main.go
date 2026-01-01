bdb6b0af5924fccac2bfae3b1553a9e4671074a3
justym <justym8132@gmail.com>
2026-01-02 02:47:06 +0900

    fix

--- main.go
+++ main.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"io"
+	"log"
 	"os"
 )
 
 func main() {
 	input, err := io.ReadAll(os.Stdin)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error reading stdin: %v\n", err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
-
-	output := Convert(string(input))
-	fmt.Print(output)
+	os.Stdout.WriteString(Convert(string(input)))
 }
